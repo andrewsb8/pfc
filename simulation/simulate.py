d557c938b4d566ee82e6195202fd35202abedfb2
bandrews <b9andrews@gmail.com>
2026-05-22 09:54:36 -0400

    add mpi to requirements and log fatal traces

--- simulation/simulate.py
+++ simulation/simulate.py
@@ -2,4 +2,7 @@ from src.PFC import PFC_Sim
 
 if __name__ == "__main__":
     pfc = PFC_Sim("config.yaml")
-    pfc._simulate()
+    try:
+        pfc._simulate()
+    except Exception:
+        pfc.log.exception("------ Fatal Error Stack Trace ------")
