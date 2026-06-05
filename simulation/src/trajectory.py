bea063f5e374d25efef1cf0d9f2fb984a4c2b6b8
bandrews <b9andrews@gmail.com>
2026-06-05 13:14:47 -0400

    add information to metadata describing how full the preallocated dataset is

--- simulation/src/trajectory.py
+++ simulation/src/trajectory.py
@@ -10,16 +10,22 @@ class TrajectoryWriter(object):
         self._store_attribute("time", str(time))
         self._store_attribute("parameters", json.dumps(config))
         self._store_attribute("mesh", mesh_content)
+        self._store_attribute("steps_written", "0")
+        self.steps_written = 0
 
     def _create_dataset(self, dset_shape):
         self.traj_file.traj = self.traj_file.create_dataset(
-            "trajectory", shape=dset_shape, dtype="float16"
+            "trajectory",
+            shape=dset_shape,
+            dtype="float16",
         )
 
     def _store_attribute(self, name, content):
         self.traj_file.traj.attrs[name] = content
 
     def _write_data(self, step, data):
+        self.steps_written += 1
+        self.traj_file.traj.attrs["steps_written"] = str(self.steps_written)
         self.traj_file.traj[step] = data
         self.traj_file.flush()
 
